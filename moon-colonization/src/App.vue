9acc69dda2578844e7f80968fb9325b52d384981
Andrew <andrejnikitin974@gmail.com>
2025-05-17 01:11:11 +0400

    cookie auth

--- moon-colonization/src/App.vue
+++ moon-colonization/src/App.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { useComponentStore } from './stores/componentStore';
+import userApi from './components/user/userApi';
 
 const componentStore = useComponentStore()
+
+const checkTokenFromCookie = (): boolean => {
+  const cookies = document.cookie.split(';');
+  const tokenCookie = cookies.find(cookie => cookie.trim().startsWith('refresh_token'));
+  if (tokenCookie) {
+    userApi.refresh();
+    return true;
+  }
+  return false;
+};
+
+onMounted(() => {
+  const isLogged: boolean = checkTokenFromCookie();
+  if (isLogged) {
+    componentStore.setComponent('zoneChooser');
+  }
+})
 </script>
 
 <template>
