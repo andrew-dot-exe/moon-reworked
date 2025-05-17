3b4ab48f37b07d456b152ecebac4febd039a9cad
DimaNik5 <dima-nikiforov05@mail.ru>
2025-05-17 23:15:52 +0400

    docs-site +- 90%

---
+++ documentation-site/docs/.vitepress/components/ThemeIcon.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <img
+      :src="srcsvg || '/img/resources/garbage.svg'"
+      :alt="alts || 'иконка'"
+      class="inline-icon"
+    />
+  </template>
+  
+  <script setup>
+  
+  defineProps({
+    srcsvg: String,
+    alts: String
+  })
+  </script>
+  
+  <style scoped>
+      .inline-icon {
+          filter: invert(0);
+          display: inline;
+          vertical-align: middle;
+          height: 1.2em;
+      }
+  
+      .dark .inline-icon {
+          filter: invert(1);
+      }
+  </style>
+  
--- documentation-site/docs/.vitepress/theme/index.js
+++ documentation-site/docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import Collapse from '../components/Collapse.vue' // Путь к компоненту
+import ThemeIcon from '../components/ThemeIcon.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component('Collapse', Collapse) // Регистрация компонента
+    app.component('ThemeIcon', ThemeIcon)
   }
 }
